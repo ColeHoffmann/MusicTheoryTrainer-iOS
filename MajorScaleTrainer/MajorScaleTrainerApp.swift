66a09ddd74aecf0b9b21136c804dda7bd0610fc8
chhoffm <cole_hoffmann@anfcorp.com>
2026-01-07 14:06:10 -0500

    Initial Commit

---
+++ MajorScaleTrainer/MajorScaleTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MajorScaleTrainerApp.swift
+//  MajorScaleTrainer
+//
+//  Created by Cole Hoffmann on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MajorScaleTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
